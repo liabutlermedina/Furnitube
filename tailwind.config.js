8708144bec5e05bea51be794c39eb08067f71092
liabutlermedina <43002661+liabutlermedina@users.noreply.github.com>
2020-09-26 18:06:14 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      zIndex: {
+        auto: 'auto',
+        '0': '0',
+        '10': '10',
+        '20': '20',
+        '30': '30',
+        '40': '40',
+        '50': '50',
+        '-10': '-10',
+      },
+      fontFamily: {
+        logo: ['Righteous'],
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
